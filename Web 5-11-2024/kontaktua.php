4eece59ab768722d4a74a94e3f845fa220df0e94
OierCalvo <oie_cal_arr@goierrieskola.org>
2025-02-04 10:19:28 +0100

    koloreak

--- Web 5-11-2024/kontaktua.php
+++ Web 5-11-2024/kontaktua.php
@@ -6,6 +6,7 @@
     <title>Kontaktua</title>
     <link rel="stylesheet" type="text/css" href="revivatech.css" media="screen" />
     <link rel="icon" type="image/png" href="Logoa.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
